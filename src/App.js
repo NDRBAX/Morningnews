31e55613cb67142d9613997a880f68c5dd9b3f16
NDRBAX <andre.bacaliu@icloud.com>
2022-04-26 00:04:51 +0200

    Part one first init.

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./App.css";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+
+// Importer les composants à utiliser (nom reste libre)
+import ScreenHome from "./ScreenHome";
+import ScreenArticlesBySource from "./ScreenArticlesBySource";
+import ScreenMyArticles from "./ScreenMyArticles";
+import ScreenSource from "./ScreenSource";
+
+
+function App() {
+  return (
+   
+      <Router>
+        <Switch>
+          <Route component={ScreenHome} path="/" exact />
+          <Route component={ScreenSource} path="/screensource" exact />
+          <Route
+            component={ScreenArticlesBySource}
+            path="/screenarticlesbysource/:id"
+            exact
+          />
+          <Route component={ScreenMyArticles} path="/screenmyarticles" exact />
+        </Switch>
+      </Router>
+  
+  );
+}
+
+export default App;
